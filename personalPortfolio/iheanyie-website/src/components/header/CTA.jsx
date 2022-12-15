9d83bf21784478e802d194374fdba3b2dd4f57d1
iheanyiekwe <contactiheanyi@gmail.com>
2022-12-14 23:09:40 -0600

    react portfolio site

---
+++ personalPortfolio/iheanyie-website/src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CV from '../../assets/cv.pdf'
+
+const CTA = () => {
+  return (
+    <div className='CTA'>
+      <a href={CV} download>Download CV</a>
+      <a href="#contact">Let's Chat</a>
+    
+    </div>
+  )
+}
+
+export default 'CTA'
