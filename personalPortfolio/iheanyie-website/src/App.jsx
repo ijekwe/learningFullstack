e9760ce5d7978ba884f8bfba2cbfc5ae78a5556d
iheanyiekwe <contactiheanyi@gmail.com>
2022-12-15 23:49:53 -0600

    eslint warning

--- personalPortfolio/iheanyie-website/src/App.jsx
+++ personalPortfolio/iheanyie-website/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
--- personalPortfolio/iheanyie-website/src/components/header/CTA.jsx
+++ personalPortfolio/iheanyie-website/src/components/header/CTA.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 import CV from '../../assets/cv.pdf'
+
+
 
 const CTA = () => {
   return (
     <div className='CTA'>
-      <a href={CV} download>Download CV</a>
+      <a href= {CV} download>Download CV</a>
       <a href="#contact">Let's Chat</a>
-    
     </div>
   )
 }
