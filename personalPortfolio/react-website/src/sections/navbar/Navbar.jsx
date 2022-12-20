9f0c4502ac43e308ea2c491cca80d7c6f58785f9
iheanyiekwe <contactiheanyi@gmail.com>
2022-12-20 00:19:59 -0600

    navbar-css

--- personalPortfolio/react-website/src/sections/navbar/Navbar.jsx
+++ personalPortfolio/react-website/src/sections/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import Logo from '../../assets/assets/logo.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav_container">
+      <a href="index.html" className='nav_logo'>
+      <img src={Logo} alt="Logo" />
+      </a>
+      <ul className='nav_menu'>
+        {
+          data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+        }
+      </ul>
+      <button id='theme_icon'><IoIosColorPalette></IoIosColorPalette></button>
+      </div> 
+    </nav>
   )
 }
 
