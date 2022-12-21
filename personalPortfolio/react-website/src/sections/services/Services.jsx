21bf0c5b7155b973454354bb57b025c254b176c5
iheanyiekwe <contactiheanyi@gmail.com>
2022-12-21 17:33:30 -0600

    services section

--- personalPortfolio/react-website/src/sections/services/Services.jsx
+++ personalPortfolio/react-website/src/sections/services/Services.jsx
@@ -1,8 +1,27 @@
+
+import Card from '../../components/Card'
+import data from './data'
 import './services.css'
 
 const Services = () => {
   return (
-    <section id="services"><h2>Services</h2></section>
+    <section id="services">
+    <h2>Services</h2>
+    <p>I give you the best in all the services below</p>
+    <div className="container services_container">
+      {
+        data.map(item => (
+          <Card key={item.id} className="service light">
+            <div className="service_icon">{item.icon}</div>
+            <div className="service_details">
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </Card>
+        ))
+      }
+      </div>
+    </section>
   )
 }
 
---
+++ personalPortfolio/react-website/src/sections/services/data.js
@@ -0,0 +1,13 @@
+import {SiAdobexd} from 'react-icons/si'
+import {RiReactjsLine} from 'react-icons/ri'
+import {FaServer} from 'react-icons/fa'
+import {AiFillAppstore} from 'react-icons/ai'
+
+const data = [
+    {id: 1, icon: <SiAdobexd/> ,title: 'UI/UX Design', desc: 'My designs are modern and intuitive.'},
+    {id: 2, icon: <RiReactjsLine/> ,title: 'Frontend Development', desc: 'Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop'},
+    {id: 3, icon: <FaServer/> ,title: 'Backend Development', desc: 'The Security of your business/ product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks.'},
+    {id: 4, icon: <AiFillAppstore/> ,title: 'App Development', desc: 'I will build an app that runs on both Android and IOS devices without extra cost to you.'}
+]
+
+export default data
